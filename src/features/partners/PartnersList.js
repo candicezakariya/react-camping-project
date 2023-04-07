14e8a8f81c976c3b88f7351fe5ef3ea263d36836
candicezakariya <candicelzakariya@gmail.com>
2023-04-06 21:45:11 -0500

    Redux for All

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,19 +1,22 @@
-import { Col } from 'reactstrap';
+import { useSelector } from 'react-redux';
+import { Col, Row } from 'reactstrap';
 import { PARTNERS } from '../../app/shared/PARTNERS';
 import Partner from './Partner';
 import { selectAllPartners } from './partnersSlice';
 
-const PartnersList = () => {
-    const partners = selectAllPartners();
+const PartnersList = (state) => {
+    const partners = useSelector(selectAllPartners);
     return (
         <Col className='mt-4'>
-            {partners.map((partner) => {
-                return (
-                    <div className='d-flex mb-5' key={partner.id}>
-                        <Partner partner={partner}/>
-                    </div>
-                );
-            })}
+            <Row>
+                {partners.map((partner) => {
+                    return (
+                        <div className='d-flex mb-5' key={partner.id}>
+                            <Partner partner={partner}/>
+                        </div>
+                    );
+                })}
+            </Row>
         </Col>
     );
 };
